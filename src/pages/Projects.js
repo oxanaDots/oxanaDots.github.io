b3cdbe36c0a96765b00d5f36ae3356ea5d15506e
Oksana Dotsenko <oksanadotsenko967@gmail.com>
2025-09-08 10:13:45 +0100

    new

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -34,11 +34,10 @@ function Projects(){
 
     link='https://photo-platform-git-main-oxanas-projects-46ce71a7.vercel.app/'
     imagePath='/images/screely-1751064545350.png'
-    programmingLngs={['React', 'Tailwind', 'Node.js', "MySQL", "Typescript" , "Javascript"]}
-   
+    programmingLngs={['React', 'Tailwind', 'Node.js', "MySQL", "Express.js", "Typescript" , "Javascript"]}
     />
     <Project
-
+//adding some new code
     link='https://final-project-red-delta.vercel.app/'
     imagePath='/images/screely-1753725633136.png'
     programmingLngs={['React', 'Tailwind', 'Node.js', "Firebase", , "Javascript"]}
